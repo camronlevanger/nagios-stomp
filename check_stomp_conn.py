d87451c8fc8b3ea95a801664b5d850df972e8823
Camron Levanger <clevanger@insidesales.com>
2014-07-29 14:29:57 -0600

    Added full connection args parsing, exception handling for traceback, changed the exit messages to look better in Nagios, and fixed some dprint problems

--- check_stomp_conn.py
+++ check_stomp_conn.py
@@ -5,10 +5,17 @@ import traceback
 import argparse
 import time
 import stomp
+import logging
 
+logging.basicConfig(level=logging.DEBUG)
 exit_code = 3
 exit_message = 'UNKNOWN - Unable to get info for stomp connections'
 debug = False
+
+def dprint(message):
+    global debug
+    if debug:
+        print message
 
 
 class MyListener(object):
@@ -18,21 +25,21 @@ class MyListener(object):
         global exit_code
         global exit_message
         exit_code = 1
-        exit_message = 'WARNING - stuck in connection state to: %s', host_and_port
+        exit_message = 'WARNING - stuck in connection state to'
 
     def on_error(self, headers, message):
         dprint(message)
         global exit_code
         global exit_message
         exit_code = 2
-        exit_message = 'CRITICAL - error connecting to stomp server: %s, %s', headers, message
+        exit_message = 'CRITICAL - error connecting to stomp server'
 
     def on_connected(self, headers, body):
         dprint(body)
         global exit_code
         global exit_message
         exit_code = 0
-        exit_message = 'OK - successfully connected to stom server: %s', message
+        exit_message = 'OK - successfully connected to stomp server'
 
 
 if __name__ == '__main__':
@@ -49,31 +56,38 @@ if __name__ == '__main__':
 
     # connection defaults
     sthost = 'localhost'
-    stport = 61680
+    stport = 61613
     stuser = 'admin'
     stpass = 'admin'
-    stopic = 'ps_notifications'
+    stopic = 'default_topic'
 
     # if args were passed in use them rather than defaults
-    if args.debug != None:
-        global debug
+    if args.debug:
         debug = True
         dprint('debug output enabled')
+    else:
+        # we are not in debug mode so override the logging level
+        logging.disable(logging.CRITICAL)
+
     if args.host != None:
         sthost = args.host
-        dprint('using host: %s', sthost)
+        dprint('using host: ' + sthost)
+
     if args.port != None:
         stport = args.port
-        dprint('using port: %s', stport)
+        dprint(('using port: ' + stport))
+
     if args.user != None:
         stuser = args.user
-        dprint('using user: %s', stuser)
+        dprint(('using user: ' + stuser))
+
     if args.password != None:
         stpass = args.password
-        dprint('using password: %s', stpass)
+        dprint(('using password: ' + stpass))
+
     if args.topic != None:
         stopic = args.topic
-        dprint('using topic: %s', stopic)
+        dprint(('using topic: ' + stopic))
 
     try:
         dprint('attempting to setup connection to stomp server')
@@ -89,16 +103,11 @@ if __name__ == '__main__':
         conn.disconnect()
     except KeyboardInterrupt:
         print "Shutdown requested...exiting"
-    except Exception:
+    except Exception as e:
+        exit_code = 2
+        exit_message = 'CRITICAL - exception raised while connecting to stomp server'
         traceback.print_exc(file=sys.stdout)
 
     # exit with exit_message and exit_code for nagios
-    global exit_code
-    global exit_message
     print exit_message
     sys.exit(exit_code)
-
-def dprint(message):
-    global debug
-    if debug:
-        print message
